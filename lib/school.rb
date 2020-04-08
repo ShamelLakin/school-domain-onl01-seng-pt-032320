1e06666d3301f15b269353dd83f510bafa97a56f
Shamel Lakin <christlife4u@gmail.com>
2020-04-08 20:40:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
+require 'pry'
 # code here!
+class School 
+  attr_accessor :name, :roster
+ 
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    binding.pry
+    # student = self.map{|n, g| }
+    @roster << student 
+  end 
+  
+end 
