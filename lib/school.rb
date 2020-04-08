12f97c7f4db2698364a749b0714d1b8c20087e9a
Shamel Lakin <christlife4u@gmail.com>
2020-04-08 21:02:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
+require 'pry'
 # code here!
+class School 
+  attr_accessor :name, :roster
+ 
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+    # binding.pry
+  end 
+  
+  def grade(grade)
+   @roster[grade]
+  end 
+  
+  def sort 
+    @roster.each {|key, value| value.sort!}
+  end
+  
+end 
